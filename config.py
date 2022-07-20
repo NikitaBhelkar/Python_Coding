d2dc668d2eef5956f84c141a64bec75603b33446
NikitaBhelkar <102810045+NikitaBhelkar@users.noreply.github.com>
2022-07-20 21:09:20 +0200

    Superpy assignment

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.abspath(os.curdir)
+bought_file = ROOT_DIR+"/data/bought.csv"
+sold_file = ROOT_DIR+"/data/sold.csv"
+products_instock_after_sell = ROOT_DIR+"/data/products_instock_after_sell.csv"
+bought_file_copy = ROOT_DIR+"/data/bought_file_copy.csv"
